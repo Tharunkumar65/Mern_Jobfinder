5625d8e8ac7a9508b3c2899f47258e3684d831b9
Duggineni Tharun kumar <dugginenitharunkumar25@gmail.com>
2024-01-31 22:17:44 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -121,7 +121,7 @@ const Signup = () => {
     
                     <div className="text-grey-dark mt-6">
                         Already have an account? 
-                        <a className="no-underline border-b border-blue text-blue" href="/login">
+                        <a className="no-underline border-b border-blue text-blue" href="https://jobfinder-three.vercel.app/login">
                             Log in
                         </a>.
                     </div>
