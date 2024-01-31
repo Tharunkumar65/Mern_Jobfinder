f38da882992e852d91267d232a6363649425554a
Tharun <dugginenitharunkumar25@gmail.com>
2024-01-31 18:55:08 +0530

    change in vercel file

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://jobfinder-three.vercel.app/",
     methods:["GET,PUT,PATCH,POST,DELETE"]
 }))
 app.use('/api/jobs',jobsrouter)
